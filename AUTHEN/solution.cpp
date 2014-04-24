d2f86529ae6498e56166a6e7068ec818db9eea03
Garvit Sharma <garvits45@gmail.com>
2014-04-24 08:16:57 -0700

    Update solution.cpp

--- AUTHEN/solution.cpp
+++ AUTHEN/solution.cpp
@@ -10,51 +10,49 @@ char c,s[N+K];
 
 int main() {
   
-    scanf("%d",&t);
-	  while (t--) {
+	scanf("%d",&t);
+	while (t--) {
     
-	  scanf("%d %d",&n,&k);
+    scanf("%d %d",&n,&k);
     scanf("%s",s+1);
     
-	  for (i=0;i<=n+k;i++)
-      for (j=0;j<=k;j++) 
-		    f[i][j] = g[i][j] = 0;
+    for (i=0;i<=n+k;i++)
+		for (j=0;j<=k;j++) 
+			f[i][j] = g[i][j] = 0;
     
-	  for (c='a';c<='z';c++) 
-		  last[c] = -1;
+    for (c='a';c<='z';c++) 
+        last[c] = -1;
     
-	  f[0][0] = g[0][0] = 1;
-	
+    f[0][0] = g[0][0] = 1;
+    
     for (i=1;i<=n+k;i++) 
-	  {
-      for (j=0; j<=k && j<=i; j++)
-		  {
-        f[i][j] = g[i-1][j];
-        
-        if (last[s[i]] >= i-j) {
-          f[i][j] -= g[last[s[i]]-1][j-(i-last[s[i]])];
+    {
+		for (j=0; j<=k && j<=i; j++)
+		{
+			f[i][j] = g[i-1][j];
+			if (last[s[i]] >= i-j) {
+				f[i][j] -= g[last[s[i]]-1][j-(i-last[s[i]])];
 
-				  if (f[i][j] < 0) f[i][j] += md;
-        }
+				if (f[i][j] < 0) f[i][j] += md;
+			}
         
-        g[i][j] = f[i][j];
+			g[i][j] = f[i][j];
         
-			  if (j > 0) 
-			  {
-          g[i][j] += g[i-1][j-1];
-		  		if (g[i][j] >= md) g[i][j] -= md;
-        }
-      }
-      	
-    	last[s[i]] = i;
+			if (j > 0) 
+			{
+				g[i][j] += g[i-1][j-1];
+				if (g[i][j] >= md) g[i][j] -= md;
+			}
+		}
+		last[s[i]] = i;
     }
-	
+    
     ans = md-1;
     for (i=n;i<=n+k;i++) {
-    	ans = ans+f[i][i-n];
-    	if (ans >= md) ans -= md;
+		ans = ans+f[i][i-n];
+        if (ans >= md) ans -= md;
     }
     printf("%d\n",ans);
-	  }
-  	return 0;
+    }
+    return 0;
 }
